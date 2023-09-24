faba8f03647ac0406c7863f2f40e9a02e5f018d4
vijayaganapathy-pavithraa <vijayaganapathy_pavithraa@s2023.ssts.edu.sg>
2023-09-25 07:52:21 +0800

    Initial Commit

---
+++ VotingApp.2/VotingApp_2App.swift
@@ -0,0 +1,17 @@
+//
+//  VotingApp_2App.swift
+//  VotingApp.2
+//
+//  Created by Vijayaganapathy Pavithraa on 25/9/23.
+//
+
+import SwiftUI
+
+@main
+struct VotingApp_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
